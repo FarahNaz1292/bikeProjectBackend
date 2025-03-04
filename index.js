59f3beda1b5d30a47e91d5ba6d6b8f329ad4d1f2
Farah naz <farahnaz@Mac.lan>
2025-03-04 00:07:06 -0500

    Changed bycrypt dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var cors = require("cors");
 const app = express();
-const port = 5000;
+const port = 5001;
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
